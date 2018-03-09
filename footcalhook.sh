c3f2d54730bd5f7f6f7959e9553a83568e1a4775
Hoegie <mezenhof8@gmail.com>
2018-03-09 15:12:07 +0100

    1st commit

---
+++ footcalhook.sh
@@ -0,0 +1,3 @@
+date >> /app/nodeprojects/github/logs/footcalhook_5002.log 2>&1
+cd /app/nodeprojects/github/footCalClientfcHerenthout
+git pull https://github.com/Hoegie/footCalClientfcHerenthout.git >> /app/nodeprojects/github/logs/footcalhook_5002.log 2>&1
---
+++ footcalini.js
@@ -0,0 +1,9 @@
+module.exports.clubID = '2';
+module.exports.clubName = 'FC HERENTHOUT';
+module.exports.apisec = false;
+module.exports.dbname = 'FootCal_fcHerenthout';
+module.exports.apachedir = 'fcHerenthout';
+module.exports.apiport = 4002;
+module.exports.mailaccount = 'skberlaar.app@gmail.com';
+module.exports.mailpassword = "4'Hoegaarden";
+
