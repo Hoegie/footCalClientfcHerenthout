c0fc0d5127df2abdc1a8e9214a393aec9b8c9bbc
Hoegie <mezenhof8@gmail.com>
2018-06-22 09:16:06 +0200

    nth commit

--- footcalini.js
+++ footcalini.js
@@ -4,6 +4,4 @@ module.exports.apisec = false;
 module.exports.dbname = 'FootCal_fcHerenthout';
 module.exports.apachedir = 'fcHerenthout';
 module.exports.apiport = 4002;
-module.exports.mailaccount = 'skberlaar.app@gmail.com';
-module.exports.mailpassword = "4'Hoegaarden";
-
+module.exports.serveraddress = "185.224.89.139";
